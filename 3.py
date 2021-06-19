46edea20db85802a9338668f1ba0dbf10dbd0223
sandeep <sandeepchowdary1877@gmail.com>
2021-06-19 12:07:57 +0530

    3

---
+++ 3.py
@@ -0,0 +1,8 @@
+print('Sandeep');
+print()
+print("i am new to the python\n and i have been laerning it since 2 days")
+
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+print("Programming","Essentials","in",sep="***",end="...")
+print("Python")
+
