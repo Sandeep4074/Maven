6fec5e7158057e5bfea6ecaf353038361910e2c9
sandeep <sandeepchowdary1877@gmail.com>
2021-06-19 12:09:45 +0530

    5

---
+++ 5.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+print('Sandeep');
+=======
+[3;2~[3;2~print('Sandeep');
+>>>>>>> 5e1b2dc (second branch cmmt)
+print()
+print("i am new to the python\n and i have been laerning it since 2 days")
+
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+print("Programming","Essentials","in",sep="***",end="...")
+<<<<<<< HEAD
+=======
+print("Python")
+>>>>>>> 5e1b2dc (second branch cmmt)
